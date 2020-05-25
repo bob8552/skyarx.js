1a8d6deebe53a0333c7ede37f3de008b616c8dc7
Glitch (skyarx-js) <none>
2020-05-25 12:53:54 +0000

    Some fixes

--- events/wordFilter.js
+++ events/wordFilter.js
@@ -1,27 +1,29 @@
 const Discord = require("discord.js");
+const blocked = require("./blockedWords.json");
 
-const blocked = require('./blockedWords.json')
 module.exports = async (bot, message) => {
-    try {
-        if (message.author.bot) return;
-        var logchannel = message.guild.channels.cache.find(c => c.name === "logs") || bot.channels.cache.get("711376099707453480")
-        if(!logchannel)return;
+  try {
+    if (message.author.bot) return;
+    var logchannel =
+      message.guild.channels.cache.find(c => c.name === "logs") ||
+      bot.channels.cache.get("711376099707453480");
+    if (!logchannel) return;
 
-        for (let i = 0; i < blocked.length; i++) {
-            if (message.content.toLowerCase().includes(blocked[i])) {
-                const embed = new Discord.MessageEmbed()
-                    .setColor("RED")
-                    .setTitle("Flagged word !")
-                    .addFields(
-                        {name: "**User**", value:`> ${message.author.username}`},
-                        {name: "**Word**", value:`> ${message.content}`},
-                    )
-                    .setFooter(message.author.username)
-                    .setTimestamp();
-            return logchannel.send(embed)
-            }
-        }
-
-    
-    } catch (e) {logchannel.send(`There was a problem with message flagging, ${e}.`) }
-}
+    for (let i = 0; i < blocked.length; i++) {
+      if (message.content.toLowerCase().includes(blocked[i])) {
+        const embed = new Discord.MessageEmbed()
+          .setColor("RED")
+          .setTitle("Flagged word !")
+          .addFields(
+            { name: "**User**", value: `> ${message.author.username}` },
+            { name: "**Word**", value: `> ${message.content}` }
+          )
+          .setFooter(message.author.username)
+          .setTimestamp();
+        return logchannel.send(embed);
+      }
+    }
+  } catch (e) {
+    logchannel.send(`There was a problem with message flagging, ${e}.`);
+  }
+};
